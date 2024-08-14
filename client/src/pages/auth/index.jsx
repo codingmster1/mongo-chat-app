4b084b8a15cd2908a08a8fc95b23ab056f2b5d4a
Jeffrey Doyle <doylejeff98@gmail.com>
2024-08-14 13:55:59 -0400

    tailwind auth classes

--- client/src/pages/auth/index.jsx
+++ client/src/pages/auth/index.jsx
@@ -1,7 +1,11 @@
  
 const Auth = () => {
    return (
-     <div>auth</div>
+     <div className="h-[100vh] w-[100vw] flex items-center justify-center">
+        <div className="h-[80vh] bg-white border 2 border-white text-opacity-90 shadow-2xl w-[80vh] md:w-[90vw] lg:w-[70vw] xl:w-[60vw]rounded-3xl grid xl:grid-cols-2">
+
+        </div>
+     </div>
    )
  }
  
