9960d63e8602fd9c48e33cb7c134e804080a9f34
Jeffrey Doyle <doylejeff98@gmail.com>
2024-08-17 13:53:40 -0400

    usestate for password/login and installed input framework

--- client/src/pages/auth/index.jsx
+++ client/src/pages/auth/index.jsx
@@ -1,8 +1,14 @@
- import { TabsContent } from "@radix-ui/react-tabs";
+ import { Tabs, TabsContent, TabsList, TabsTrigger } from "@radix-ui/react-tabs";
 import Background from "../../assets/login2.png";
  import Victory from "../../assets/robot_logo.png";
+import { useState } from "react";
 
 const Auth = () => {
+
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [confirmPassword, setConfirmPassword] = useState("");
+
    return (
      <div className="h-[100vh] w-[100vw] flex items-center justify-center">
         <div className="h-[80vh] bg-white border-2 border-white text-opacity-90 shadow-2xl w-[80vh] md:w-[90vw] lg:w-[70vw] xl:w-[60vw]rounded-3xl grid xl:grid-cols-2">
@@ -14,10 +20,19 @@ const Auth = () => {
     <p className="font-medium text-center">Fill in the details to get started</p>
 </div>
 <div className="flex items-center justify-center w-full">
-<Tabs>
-    <TabsList>
-        <TabsTrigger value="login"></TabsTrigger>
-        <TabsTrigger value="signup"></TabsTrigger>
+<Tabs className="w-3/4">
+    <TabsList className="bg-transparent rounded-none w-full flex">
+        <TabsTrigger value="login"
+        className="data-[state=active]:bg-transparent
+        text-black text-opacity-90 border-b-2
+        rounded-none w-full data-[state=active]:text-black data-[state=active]:font-semibold data-[state=active]:border-b-purple-500 p-3 transition-all duration-300"
+        >Login</TabsTrigger>
+        <TabsTrigger 
+        value="signup"
+       className="data-[state=active]:bg-transparent
+       text-black text-opacity-90 border-b-2
+       rounded-none w-full data-[state=active]:text-black data-[state=active]:font-semibold data-[state=active]:border-b-purple-500 p-3 transition-all duration-300"
+        >Sign Up</TabsTrigger>
     </TabsList>
     <TabsContent value="login"></TabsContent>
     <TabsContent value="signup"></TabsContent>
