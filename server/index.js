f53789675b5aba5f5fc3be3a897763849efdf875
Jeffrey Doyle <doylejeff98@gmail.com>
2024-08-20 13:07:57 -0400

    server is running on server folder and app.use mongoose connected

--- server/index.js
+++ server/index.js
@@ -10,6 +10,18 @@ const app = express();
 const port = process.env.PORT || 3000;
 const databaseURL = process.env.DATABASE_URL;
 
+app.use(cors({
+    origin: process.env.ORIGIN,
+    periods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
+    credentials: true
+}));
+
+
 const server = app.listen(port, () => {
-  console.log(`Server is running on port http://localhost:${PORT}`);
+  console.log(`Server is running on port http://localhost:${port}`);
 });
+
+
+mongoose.connect(databaseURL)
+    .then(() => console.log('Connected to database'))
+    .catch((error) => console.log(error));
